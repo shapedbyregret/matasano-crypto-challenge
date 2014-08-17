bba607a625fc7d4d5c643026621178d95c3c26c0
Erik Johnson <shapedbyregret@gmail.com>
2014-08-17 13:06:13 -0500

    Think I have key size guesser correct?

--- rust/1-6.rs
+++ rust/1-6.rs
@@ -27,10 +27,10 @@ fn repeating_xor(str1:&str, key:&str) -> String {
 }
 
 // Hamming distance is the number of differing *bits*.
-fn edit_distance(str1:&str, str2:&str) -> int {
+fn edit_distance(str1:&[u8], str2:&[u8]) -> int {
     let mut diff:int = 0;
-    let str1bits:Bitv = bitv::from_bytes(str1.as_bytes());
-    let str2bits:Bitv = bitv::from_bytes(str2.as_bytes());
+    let str1bits:Bitv = bitv::from_bytes(str1);
+    let str2bits:Bitv = bitv::from_bytes(str2);
     for n in range(0, str1bits.len()) {
         if str1bits.get(n) != str2bits.get(n) {
             diff = diff + 1;
@@ -43,9 +43,28 @@ fn main() {
     // Break repeating key XOR. Input file is base64.
 
     // Test edit distance
-    let test_edit = edit_distance("this is a test", "wokka wokka!!!");
+    let test_edit = edit_distance("this is a test".as_bytes(),
+                                  "wokka wokka!!!".as_bytes());
     println!("Differing Bits: {}", test_edit);
     assert_eq!(37, test_edit);
+
+    // Load file into memory
+    let file = File::open(&Path::new("6.txt")).read_to_string().unwrap();
+
+    // Guess keysize
+    let mut key_len:int = 2;
+    let mut min_dist:f32 = 9999.0;
+    for n in range(2, 40) {
+        let slice1 = file.as_bytes().slice(0, n);
+        let slice2 = file.as_bytes().slice(n, n*2);
+        let diff = edit_distance(slice1, slice2);
+        let norm = diff as f32 / (n as f32);
+        if norm < min_dist {
+            min_dist = norm;
+            key_len = n as int;
+        }
+    }
+    println!("Guessed Key Length: {}", key_len);
 
     let path = Path::new("6.txt");
     let mut file = BufferedReader::new(File::open(&path));
